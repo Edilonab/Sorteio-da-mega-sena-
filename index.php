eff9db8b12a204781dc92b49d5d2f80a6cd59d47
Edilon Abranches <edilonab@hotmail.com>
2022-04-02 22:05:45 +0000

    parte logica

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -8,17 +9,19 @@
             $_megasena = array();
             $_contador = 1;
 
-            while ($_contador < 7) {
+            while ( $_contador < 7 ) {
                 $_sorteio = rand(1,60);
-                if(!in_array($_sorteio, $_megasena) ) {
+                if( !in_array($_sorteio, $_megasena) ) {
                     $_megasena[] = $_sorteio;
                     $_contador ++;
                 }
-            }
-        
-            log()      
+            } 
             
-        
+            sort($_megasena)
         ?>
+
+        <pre>
+            <?php print_r($_megasena) ?>
+        </pre>
     </body>
 </html>
